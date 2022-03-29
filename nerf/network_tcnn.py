7c6a7c3ee0994454f52a9f04acb76590e5fdf092
fusang <fusangwang98@gmail.com>
2022-03-29 03:59:46 -0400

    seperate nerf and nerfw using -tcnn and -tcnnw, tested on blender

--- nerf/network_tcnn.py
+++ nerf/network_tcnn.py
@@ -8,7 +8,7 @@ import tinycudann as tcnn
 from .renderer import NeRFRenderer
 
 
-class NeRFNetwork(NeRFRenderer):
+class NeRFWNetwork(NeRFRenderer):
     def __init__(self,
                  encoding="HashGrid",
                  encoding_dir="SphericalHarmonics",
@@ -109,9 +109,140 @@ class NeRFNetwork(NeRFRenderer):
 
         #p = torch.zeros_like(geo_feat[..., :1]) # manual input padding
         #h = torch.cat([d, geo_feat, l_a], dim=-1)
-        h = torch.cat([d, geo_feat], dim=-1)
+        #h = torch.cat([d, geo_feat], dim=-1)
         h_s = torch.cat([d, geo_feat, l_a], dim=-1)
-        h = self.color_net(h_s)
+        h_s = self.color_net(h_s)
+        
+        # sigmoid activation for rgb
+        color = torch.sigmoid(h_s)
+    
+        sigma = sigma.view(*prefix)
+        color = color.view(*prefix, -1)
+
+        return sigma, color
+
+    def density(self, x):
+        # x: [B, N, 3], in [-bound, bound]
+
+        prefix = x.shape[:-1]
+        x = x.view(-1, 3)
+
+        x = (x + self.bound) / (2 * self.bound) # to [0, 1]
+        x = self.encoder(x)
+        h = self.sigma_net(x)
+
+        #sigma = torch.exp(torch.clamp(h[..., 0], -15, 15))
+        sigma = F.relu(h[..., 0])
+
+        sigma = sigma.view(*prefix)
+
+        return sigma
+
+class NeRFNetwork(NeRFRenderer):
+    # will pass all elements in NeRFWNetwork but not passing l_a in forward
+    def __init__(self,
+                 encoding="HashGrid",
+                 encoding_dir="SphericalHarmonics",
+                 num_layers=2,
+                 hidden_dim=64,
+                 geo_feat_dim=15,
+                 num_layers_color=3,
+                 hidden_dim_color=64,
+                 bound=1,
+                 cuda_ray=False,
+                 in_channels_a=16,
+                 in_channels_t=16,
+                 N_vocab = 100
+                 ):
+        super().__init__(bound, cuda_ray)
+
+        # sigma network
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+        self.geo_feat_dim = geo_feat_dim
+        self.in_channels_a = in_channels_a
+        self.in_channels_t = in_channels_t
+
+        per_level_scale = np.exp2(np.log2(2048 * bound / 16) / (16 - 1))
+        self.embedding_a = torch.nn.Embedding(N_vocab, in_channels_a)
+        self.embedding_t = torch.nn.Embedding(N_vocab, in_channels_t)
+        self.encoder = tcnn.Encoding(
+            n_input_dims=3,
+            encoding_config={
+                "otype": "HashGrid",
+                "n_levels": 16,
+                "n_features_per_level": 2,
+                "log2_hashmap_size": 19,
+                "base_resolution": 16,
+                "per_level_scale": per_level_scale,
+            },
+        )
+
+        self.sigma_net = tcnn.Network(
+            n_input_dims=32,
+            n_output_dims=1 + self.geo_feat_dim,
+            network_config={
+                "otype": "FullyFusedMLP",
+                "activation": "ReLU",
+                "output_activation": "None",
+                "n_neurons": hidden_dim,
+                "n_hidden_layers": num_layers - 1,
+            },
+        )
+
+        # color network
+        self.num_layers_color = num_layers_color        
+        self.hidden_dim_color = hidden_dim_color
+
+        self.encoder_dir = tcnn.Encoding(
+            n_input_dims=3,
+            encoding_config={
+                "otype": "SphericalHarmonics",
+                "degree": 4,
+            },
+        )
+
+        self.in_dim_color = self.encoder_dir.n_output_dims + self.geo_feat_dim
+        #self.in_dim_color = self.in_channels_a + self.encoder_dir.n_output_dims + self.geo_feat_dim
+        self.color_net = tcnn.Network(
+            n_input_dims=self.in_dim_color,
+            n_output_dims=3,
+            network_config={
+                "otype": "FullyFusedMLP",
+                "activation": "ReLU",
+                "output_activation": "None",
+                "n_neurons": hidden_dim_color,
+                "n_hidden_layers": num_layers_color - 1,
+            },
+        )
+
+    
+    def forward(self, x, d, l_a, l_t):
+        # x: [B, N, 3], in [-bound, bound]
+        # d: [B, N, 3], nomalized in [-1, 1]
+
+        prefix = x.shape[:-1]
+        x = x.view(-1, 3)
+        d = d.view(-1, 3)
+        l_a = l_a.view(-1,self.in_channels_a)
+
+        # sigma
+        x = (x + self.bound) / (2 * self.bound) # to [0, 1]
+        x = self.encoder(x)
+        h = self.sigma_net(x)
+
+        sigma = F.relu(h[..., 0])
+        geo_feat = h[..., 1:]
+
+        # color
+        d = (d + 1) / 2 # tcnn SH encoding requires inputs to be in [0, 1]
+        d = self.encoder_dir(d)
+
+        #p = torch.zeros_like(geo_feat[..., :1]) # manual input padding
+        #h = torch.cat([d, geo_feat, l_a], dim=-1)
+        h = torch.cat([d, geo_feat], dim=-1)
+        #h_s = torch.cat([d, geo_feat, l_a], dim=-1)
+        h = self.color_net(h)
         
         # sigmoid activation for rgb
         color = torch.sigmoid(h)
